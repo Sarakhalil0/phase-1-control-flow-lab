ef1cf2422914a983de52f2c6b8b86b1a2273b0bf
sara khalil <sarakhalil3377@gmail.com>
2022-07-31 22:15:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(tip){
   // Write your code here!
+
+if( tip <= 400){
+  return 'This one is on me!';
+} else if( tip >= 400 && tip <= 2000){
+  return 'That will be twenty bucks.';
+}
+else if(tip > 2000 && tip < 2500)
+{
+  return 'I will gladly take your thirty bucks.';
+}
+else if(tip > 2500) {
+  return 'No can do.';
+}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  
+ return city === "NYC"?"Ok, sounds good.":"No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+switch(tip){
+case 'generous':
+  return 'Thank you so much.';
+  case 'not as generous':
+    return 'Thank you.';
+    case 'thanks for everything':
+    return 'Bye.';
+    
+}
+
 }
